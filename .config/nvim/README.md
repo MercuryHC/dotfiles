c5176f7a01ab53b900d9df8c9b7cce430aa60f82
MercuryHC <38241962+MercuryHC@users.noreply.github.com>
2025-10-17 09:53:43 +0000

    explain vim.g, vim.o/vim.opt and vim.cmd

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -14,10 +14,22 @@
 在nvim中使用命令`source %`用来加载修改后的配置
 
 2. 配置简介
-主要用到的就是 vim.g、vim.opt、vim.cmd 等，快速参照对比的表格
+主要用到的就是 vim.g、vim.opt/vim.o、vim.cmd 等，快速参照对比的表格
 
 | In Vim | In nvim | Note |
 | :---: | :---: | :---: |
 | let g:foo = bar | vim.g.foo = bar| |
 | set foo = bar | vim.opt.foo = bar| set foo = vim.opt.foo = true |
 | some_vimscript | vim.cmd(some_vimscript)| |
+
+> vim.g 用于访问全局变量，等价于：let g:xxx
+> vim.o 和 vim.opt 用于设置选项，等价于：set xxx
+> vim.cmd 用于执行vimscript命令，等价于：:xxx
+
+> vim.o和vim.opt的区别
+> vim.o 是一个全局作用域选项，等价于：set xxx
+> vim.opt 是一个现代化、面向对象接口，提供对全局、局部、缓冲区、窗口、终端等多种作用域的统一管理，常用来设置listchars等复杂选项
+> `vim.o.list = true`
+> `vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }`
+>
+> For more details, see :help lua-guide and :help vim-diff.
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,9 +1,17 @@
+-------------------------------------------------------------------------------
+-- 全局设置
 vim.g.mapleader = " "               -- 设置全局leader键为空格
 vim.g.maplocalleader = " "          -- 设置本地leader键为空格
+
 vim.g.have_nerd_font = true         -- 启用nerd_font字体
 vim.g.markdown_folding = 1          -- 启用markdown层级折叠
-vim.opt.linebreak = true            -- 启用自动换行
-vim.g.simple_indicator_on = false   --关闭简单指示器
+vim.g.simple_indicator_on = false   -- 关闭简单指示器
+
+--------------------------------------------------------------------------------
+-- [[ Setting options ]]
+-- See `:help vim.o`
+--  For more options, you can see `:help option-list`
+vim.o.linebreak = true              -- 启用自动换行
 vim.o.cursorline = true             -- 高亮当前行
 vim.o.winborder = "single"          -- 设置窗口边框样式
 vim.o.expandtab = true              -- 使用空格代替tab
@@ -11,3 +19,7 @@ vim.o.number = true                 -- 显示行号
 vim.o.tabstop = 4                   -- 设置tab宽度为4个空格
 vim.o.shiftwidth = 4                -- 设置自动缩进宽度为4个空格
 vim.o.smartindent = true            -- 启用智能缩进
+
+--------------------------------------------------------------------------------
+-- 使用vim.cmd 代替指令
+vim.cmd.colorscheme "default"       -- 设置终端主题
